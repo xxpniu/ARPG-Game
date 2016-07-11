37fe44cc386bbdcdeb7cebfb5473e13c5d53cfde
xxpniu <54249636@qq.com>
2016-07-11 23:25:59 +0800

    Signed-off-by: xxpniu <54249636@qq.com>

--- src/NetSources/Config.cs
+++ src/NetSources/Config.cs
@@ -34,63 +34,81 @@ namespace ExcelConfig
         public String ResourcesPath { set; get; }
         
         /// <summary>
-        /// 血量
+        /// AI路径
         /// </summary>
         [ExcelConfigColIndex(3)]
+        public String AIResourcePath { set; get; }
+        
+        /// <summary>
+        /// 攻击速度(间隔秒)
+        /// </summary>
+        [ExcelConfigColIndex(4)]
+        public float AttackSpeed { set; get; }
+        
+        /// <summary>
+        /// 移动速度（m/s）
+        /// </summary>
+        [ExcelConfigColIndex(5)]
+        public float MoveSpeed { set; get; }
+        
+        /// <summary>
+        /// 血量
+        /// </summary>
+        [ExcelConfigColIndex(6)]
         public int HPMax { set; get; }
         
         /// <summary>
         /// 伤害小
         /// </summary>
-        [ExcelConfigColIndex(4)]
+        [ExcelConfigColIndex(7)]
         public int DamageMin { set; get; }
         
         /// <summary>
         /// 伤害大
         /// </summary>
-        [ExcelConfigColIndex(5)]
+        [ExcelConfigColIndex(8)]
         public int DamageMax { set; get; }
         
         /// <summary>
         /// 攻击力
         /// </summary>
-        [ExcelConfigColIndex(6)]
+        [ExcelConfigColIndex(9)]
         public int Attack { set; get; }
         
         /// <summary>
         /// 防御力
         /// </summary>
-        [ExcelConfigColIndex(7)]
+        [ExcelConfigColIndex(10)]
         public int Defance { set; get; }
         
         /// <summary>
         /// 等级
         /// </summary>
-        [ExcelConfigColIndex(8)]
+        [ExcelConfigColIndex(11)]
         public int Level { set; get; }
         
         /// <summary>
         /// 种族
         /// </summary>
-        [ExcelConfigColIndex(9)]
+        [ExcelConfigColIndex(12)]
         public int BodyType { set; get; }
         
         /// <summary>
         /// 防御类型
         /// </summary>
-        [ExcelConfigColIndex(10)]
+        [ExcelConfigColIndex(13)]
         public int DefanceType { set; get; }
         
         /// <summary>
         /// 攻击类型
         /// </summary>
-        [ExcelConfigColIndex(11)]
+        [ExcelConfigColIndex(14)]
         public int AttackType { set; get; }
         
         /// <summary>
         /// 攻击类型
         /// </summary>
-        [ExcelConfigColIndex(12)]
+        [ExcelConfigColIndex(15)]
         public int DamageType { set; get; }
 
     }
@@ -114,9 +132,33 @@ namespace ExcelConfig
         public String MagicKey { set; get; }
         
         /// <summary>
-        /// CoolDown时间秒
+        /// 释放最小距离
         /// </summary>
         [ExcelConfigColIndex(3)]
+        public float ReleaseRangeMin { set; get; }
+        
+        /// <summary>
+        /// 释放距离（释放最大距离）
+        /// </summary>
+        [ExcelConfigColIndex(4)]
+        public float ReleaseRangeMax { set; get; }
+        
+        /// <summary>
+        /// 释放类型
+        /// </summary>
+        [ExcelConfigColIndex(5)]
+        public int ReleaseType { set; get; }
+        
+        /// <summary>
+        /// 释放参数
+        /// </summary>
+        [ExcelConfigColIndex(6)]
+        public String ReleaseParams { set; get; }
+        
+        /// <summary>
+        /// CoolDown时间秒
+        /// </summary>
+        [ExcelConfigColIndex(7)]
         public float TickTime { set; get; }
 
     }
